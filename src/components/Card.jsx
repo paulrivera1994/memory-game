30efc9e9a9fec0244cfc43c7e806c6a68a656aa3
Paul <juniorrivera81@yahoo.com>
2023-02-14 09:19:55 -0700

    initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import head from "../image/Person.svg";
+import "./Card.css";
+
+const Card = ({ card, handleCardClick, disabled }) => {
+  return (
+    <button
+      className={`card ${card.matchFound ? "matched" : ""}`}
+      disabled={disabled}
+      onClick={handleCardClick}
+      data-id={card.id}
+    >
+      <div className="front side">
+        <img src={head} alt="head" width="60" />
+      </div>
+      <div className="side back">{card.emoji}</div>
+    </button>
+  );
+};
+export default Card;

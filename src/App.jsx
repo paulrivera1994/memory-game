79a7cd2222d1aff907e6afbc9d78582fa5d6e65d
Paul <juniorrivera81@yahoo.com>
2023-03-02 13:26:56 -0700

    reset cards duration extended

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
 
   function resetCards() {
     const shuffled = [...items, ...items]
-      .sort(() => Math.random() - 0.8)
+      .sort(() => Math.random() - 1)
       .map((card) => ({ ...card, key: Math.random() }));
     setCards(shuffled);
   }
